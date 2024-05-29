0ecf9deac61a18e7762d65e22e1bfa9bb1be74b1
ATHARV TYAGI <73381314+atharvty@users.noreply.github.com>
2024-05-29 19:02:25 +0530

    Updating neat code for MajorityElement2.cpp

--- Arrays/MajorityElement2.cpp
+++ Arrays/MajorityElement2.cpp
@@ -1,55 +1,48 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        
-        int sz = nums.size();
-        int num1 = -1 ,  num2 = -1 ,count1 = 0 , count2 = 0 , i ;
-        
-        for(i = 0 ; i < sz ; i++){
-            if(nums [i] ==num1)
-                count1++;
-            else if (nums [i] == num2)
-                count2++;
-            else if (count1==0){
-                num1 = nums[i];
-                count1 = 1;
-                
+        int n = nums.size(); //size of the array
+
+        int cnt1 = 0, cnt2 = 0; // counts
+        int el1 = INT_MIN; // element 1
+        int el2 = INT_MIN; // element 2
+
+        // applying the Extended Boyer Moore's Voting Algorithm:
+        // Works on the cancelling way to keep count of things 
+        for (int i = 0; i < n; i++) {
+            if (cnt1 == 0 && el2 != nums[i]) {
+                cnt1 = 1;
+                el1 = nums[i];
             }
-            else if(count2 == 0 ){
-                num2 = nums[i];
-                count2 = 1;
+            else if (cnt2 == 0 && el1 != nums[i]) {
+                cnt2 = 1;
+                el2 = nums[i];
             }
-            else{
-                count1--;
-                count2--;
-                
-            }
-        }
-        vector <int> ans;
-        count1 = count2 = 0 ;
-        for(int i = 0 ; i<sz ; i++ ){
-            if(nums[i] == num1){
-                count1 ++;
-            }
-            else if (nums[i] == num2){
-                count2 ++;
+            else if (nums[i] == el1) cnt1++;
+            else if (nums[i] == el2) cnt2++;
+            else {
+                cnt1--, cnt2--;
             }
         }
-        if(count1 >sz/3){
-            ans.push_back(num1);
-            
+
+        vector<int> ls; // list of answers
+
+        // Manually check if the stored elements in
+        // el1 and el2 are the majority elements:
+        cnt1 = 0, cnt2 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == el1) cnt1++;
+            if (nums[i] == el2) cnt2++;
         }
-        if(count2 >sz/3){
-            ans.push_back(num2);
-            
-        
-            
-        }
-        return ans;
-        
-        
-        
-        
-        
+
+        int mini = int(n / 3) + 1;
+        if (cnt1 >= mini) ls.push_back(el1);
+        if (cnt2 >= mini) ls.push_back(el2);
+
+        // Uncomment the following line
+        // if it is told to sort the answer array:
+        // sort(ls.begin(), ls.end()); //TC --> O(2*log2) ~ O(1);
+
+        return ls;
     }
 };
